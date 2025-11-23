6f5f176bc68c45f4c6fbafba48dcbe3f8355183b
Lob2018 <@>
2025-11-23 17:21:58 +0100

    🔧 chore: Change project license from MIT to GPLv3.0 - Moved from v0.11.1.0 to v0.12.0.0 - Tests pass - Doc Doxygen updated

--- src/main/java/fr/softsf/canscan/service/VersionService.java
+++ src/main/java/fr/softsf/canscan/service/VersionService.java
@@ -1,6 +1,6 @@
 /*
  * CanScan - Copyright © 2025-present SOFT64.FR Lob2018
- * Licensed under the MIT License (MIT).
+ * Licensed under the GNU General Public License v3.0 (GPLv3.0).
  * See the full license at: https://github.com/Lob2018/CanScan?tab=License-1-ov-file#readme
  */
 package fr.softsf.canscan.service;
